cf76ba6090ecd099afedfc201af01ad249ec81d3
Vitor Oliveira <vitormil@gmail.com>
2025-10-22 21:21:01 +0100

    Updates

--- common/try.fish
+++ common/try.fish
@@ -1,9 +1,29 @@
-#########
-## try ##
-#########
-
-# try is a lightweight shell script that gives each of your
-# quick experiments a clean, dedicated directory.
+#!/usr/bin/env fish
+# -----------------------------------------------------------------------------
+# Project: try - a lightweight shell script that gives each of your experiments a dedicated directory.
+# Author: Vitor Oliveira
+# License: MIT
+#
+# Copyright (c) 2025 Vitor Oliveira
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the “Software”), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# -----------------------------------------------------------------------------
 
 # Instead of cluttering your system with test, test2, and final-test,
 # it instantly spins up a fresh workspace and drops you right in—keeping
@@ -12,7 +32,7 @@
 # Inspired by https://github.com/tobi/try
 
 # Usage:
-#   try                # list and cd into existing projects
+#   try               # list and cd into existing projects
 #   try add|new       # create a new project
 #   try remove|rm     # remove an existing project
 #   try <query>       # fuzzy search and cd into an existing project
