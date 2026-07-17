3b057070aa9864cc0dd7eb03b7f10e27ec8d0a1d
Vitor Oliveira <vitormil@gmail.com>
2026-07-17 16:33:05 +0100

    Update node to 24.18.0 and add npm 11.16.0, remove M2_HOME export

--- common/.config/fish/conf.d/exports.fish
+++ common/.config/fish/conf.d/exports.fish
@@ -6,8 +6,6 @@ export BAT_THEME="ansi"
 export PGUSER="postgres"
 export PGPASSWORD="docker"
 
-export M2_HOME=(mvn -version | awk -F': ' '/Maven home/{print $2}')
-
 export TODO_FILE="$HOME/vitor/TODO.txt"
 
 export BAT_THEME="Catppuccin Mocha" # bat cache --build
